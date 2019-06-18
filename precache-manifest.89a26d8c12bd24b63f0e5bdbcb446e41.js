4f0b0d87ab3c23e87176df8fec2ec2d486250e01
Firastic <adyaksa.wisanggeni@gmail.com>
2019-06-18 19:25:47 +0700

    Updates

---
+++ precache-manifest.89a26d8c12bd24b63f0e5bdbcb446e41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b7eb75f134c68d6a0d531dc4ead7bb",
+    "url": "/nim-finder/index.html"
+  },
+  {
+    "revision": "2acceb3f1b7c360e099f",
+    "url": "/nim-finder/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "9f0f47cc047dd2ddec08",
+    "url": "/nim-finder/static/js/2.3d3e5e3a.chunk.js"
+  },
+  {
+    "revision": "2acceb3f1b7c360e099f",
+    "url": "/nim-finder/static/js/main.194295b3.chunk.js"
+  },
+  {
+    "revision": "bcf8370f0404171e2f85",
+    "url": "/nim-finder/static/js/runtime~main.5854a2c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nim-finder/static/media/logo.5d5d9eef.svg"
+  }
+]);
